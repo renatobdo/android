b1927429a9815acc3b15d6d86ce33f395b21e02e
Renato Bueno <renatobdo@gmail.com>
2021-12-09 10:40:54 -0300

    Update MainActivity.java

--- RecuperarCampoTexto/MainActivity.java
+++ RecuperarCampoTexto/MainActivity.java
@@ -1,7 +1,7 @@
 package com.ifsp.recuperartexto;
 
 import androidx.appcompat.app.AppCompatActivity;
-import android.widget.EditText;
+import android.widget.*;
 import android.os.Bundle;
 import android.view.View;
 
@@ -13,10 +13,14 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
     public void enviar(View view){
-        EditText nome = findViewById(R.id.editTextNome);
-        TextView resultado = findViewById(R.id.textViewResultado);
+        EditText nomeEdit = findViewById(R.id.editTextNome);
+       
+        TextView resultadoTextView = findViewById(R.id.textViewResultado);
 
+        String nome = nomeEdit.getText().toString();
         
+
+        resultadoTextView.setText("nome = "+nome);
 
 
     }
